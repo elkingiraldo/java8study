dd068d1d01710b7b38cf2e965a677b81c4bd71ef
Elkin Giraldo <elkin.giraldo.pinedo@gmail.com>
2022-07-17 23:37:46 -0500

    :construction: add palindrome class

---
+++ Java8Knowledge/src/co/learn/java8/elkin/spotify/Palindrome.java
@@ -0,0 +1,14 @@
+package co.learn.java8.elkin.spotify;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println(buildPalindrome("bac", "bac"));
+        System.out.println(buildPalindrome("abc", "def"));
+        System.out.println(buildPalindrome("jdfh", "fds"));
+    }
+
+    private static String buildPalindrome(String a, String b) {
+        return "test";
+    }
+}
